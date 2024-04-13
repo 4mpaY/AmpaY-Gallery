d9c7f39557d38fea0352d922dc5fa9da4fd0d5b7
Jose_Huerta <jose.huerta.alonso@outlook.com>
2024-04-12 19:04:21 -0500

    imagenes y orden

---
+++ imagenes.js
@@ -0,0 +1,15 @@
+const galeria=[
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczNBF7RYBhUQfglkSSG3AP9Id4f1sy2uRKQazbEnnihSEXYSCr3bJU0nnxTWX48dWbKQITyOAjvfJFCHd_hHGwdGRyB277O3J4TelZmjDwtx9OIwntqpEewpoKlipjHzi0hG_TzjBQwl8PRlnZHhqghRAw=w696-h929-s-no-gm?authuser=0'},
+  {src:'https://okdiario.com/img/2023/08/26/no-mires-arriba-la-razon-de-los-atardeceres-tenidos-de-rojos-no-esta-en-el-cielo.jpg'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczM-RdMQWtn5xD5h9JuwUUTOfEuofZr6DJSFiTO8aUtncRYJ-j-qz_oqrSTu0w7MH_Ui2DSzO811IrDqgLtHARp5HRVKLm_49XUHjVdibA5p_8kAkBwRgOqpkHga4Gm7EYqXNLh0vowekWuNXIbquuPF4Q=w1239-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczOkLke6UVlKBLqReo90m1PZNBLvUnRs769TEL6BvOEk8KnfFJJc-8igRuGLnD7UusPYdrGonRHh6RYEoGSg8T9VSDY1jP0UVClJ2XUdTuKVDx2b_8FxBtO3uBsUQVWrUIVfatrVFh7YLEbf1eo1y3mEzQ=w1239-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczNHLzuSG49ElfsciliGkmmacgoIts8gO00oGRVdpCgCQXNDZCTM_4qYoc2Db0kiCQuo37LG1shro2NZKl0HYBiQBF8cITu27GvGYQSXRN23p-MIG543v5Db0PUopEdp9QymubwLSSoX2pK2hVRfBURbEg=w1239-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczPi5y7SU1M1ERDDwEXkTJ78uUABiLAfaomf8ZRCWwev6GqbEpSPZsyd8CVIgFErwp1l1VovYUejViMnApZ_bVDEvpYQ2FfuIf249OHhgCqr720daNMX8lHNirZbvrur51AQ3dw-KsUVRcNr9F9raQFamg=w696-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczMbXpIP_KNsKLikrf7wcx84pPDJldzU-ZbmCkibCq4yTQ3o9yFoQIR5799zgssp7XJN9IrI0SKJtJ3iJHewegkXHRpDz2YUpuvf8NiUWYuOQHVQXZPsENGSuw9othzDc7i3yNr269TyAF7O0zK_azPI6g=w696-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczOnLr1XVKGtp-Mezlqdtr-S1VgBIn9vJpcplmbme7G1nVXUax2Sd1Ragyw1Kf38yE55GlHtElrF12CM4lhO1fIgebaxqTOjnqx7U7oWpZMAVksqr2ZKcBRcVGYGra_iyJ21yISpPpSzGovExvG1EJCPAA=w696-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczPH5KjlSC5wgo7pL3PGEgKvpqQhHuOO9yXPpBtySEIttjpaHV9avGAYsEzFdw7izdB2--tXwA9o7rDY1ng5kf41TQ6YRhUaCiPLktDyXQC6PDPgEy8k6RDdXkdeJJI_7g9g3q5_eIys423Fi5aADaA9kg=w696-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczM7miSnUaVLiGRCjcaVWZYulQ6AkYlwRby7Pf0PVncQGyqZc67ljf2LifOap4VbfGz60cvbG84Ypf6SD5CFMeJyfjm0XW8e9_hPhte34DbO73n5wrWvw-mvWQL41X4F7Cp_Af6grgbJ88b92ICWWUA_-Q=w695-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczN-MDixCA-2O4H62Zbv3fLJrkE4TBTsIFnYyl0DMy3w6S6cy2_vGOCA_c4b8Xq7NBJJJ8xHA-o_oJLlzKc-IKOJ_-BTQPK4IKC1WTPUPzBwZNuicnlgM85PJhpOrdJXtfIOG9CoypPsEN-0aOmxkq0kLg=w696-h929-s-no-gm?authuser=0'},
+  {src:'https://lh3.googleusercontent.com/pw/AP1GczMcxZ46Obcvp6RnIyMQCeb0S0OA8SNhuluxDO5ntPToVYUH1DtvIPva2VRxtYrpqNYiT29agUswkQw_ByNlpFbQNaVdrxg0XfAfHpFNw52DPw_byp3gcZvMi_Dis9k5OZ0ZA8IPtuWTtue4fIHxEw2b9g=w696-h929-s-no-gm?authuser=0'},
+]
+export{galeria};
