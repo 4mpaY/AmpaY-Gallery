6049864259f84f1ba2e7ad658bf47f860cf3585e
Jose_Huerta <jose.huerta.alonso@outlook.com>
2024-04-14 18:35:29 -0500

    transicion y reaccion

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ for(let i=0;i<100;i++){
   const imagen = document.createElement('img')
   const containerImg = document.createElement('div')
   containerImg.classList.add('divImagen')
-
+  imagen.classList.add('imagen')
   imagen.src = imagenSeleccionada.src
 
   containerImg.appendChild(imagen)
@@ -19,41 +19,54 @@ for(let i=0;i<100;i++){
 const lista =document.querySelectorAll('.divImagen')
 const nodeImagenes = [...lista]
 
-nodeImagenes.forEach(div=>{div.addEventListener('click', function(event){
+nodeImagenes.forEach(div=>{div.addEventListener('click', ClickImagen)})
 
+function ClickImagen(){
   const imagen = this.querySelector('img')
   const imagenCopia = document.createElement('img')
   imagenCopia.src= imagen.src
   const divNuevo = document.createElement('div')
 
-  divNuevo.classList.add('divDefecto')
+  divNuevo.classList.add('divActiva')
+  imagenCopia.classList.add('imagenActiva')
+  divNuevo.addEventListener('click', ClickBorrar)
+
+  const actividad = document.createElement('div')
+  const input = document.createElement('imput')
+  const corazon = document.createElement('img')
+
+  input.classList.add('comentario')
+  corazon.src='/icons/corazon.png'
+  actividad.classList.add('actividad')
+  corazon.classList.add('reacciones')
+
+
+  actividad.appendChild(input)
+  actividad.appendChild(corazon)
+
   divNuevo.appendChild(imagenCopia)
+  divNuevo.appendChild(actividad)
   container.appendChild(divNuevo)
   
 
   
- setTimeout(function retraso (){ divNuevo.classList.add('divActiva')
-  imagenCopia.classList.add('imagenActiva')},1)
-  
-  divNuevo.addEventListener('click', borrar)
-  
-  function borrar(){ 
-    const color1 = Math.floor(Math.random()*255)
-    const color2 = Math.floor(Math.random()*255)
-    const color3 = Math.floor(Math.random()*255)
-    const color4 = Math.floor(Math.random()*10)/10
-    
-    body.style.backgroundColor= `rgb(${color1}, ${color2}, ${color3}, ${color4})`
-
-    divNuevo.classList.remove('divDefecto')
-    divNuevo.classList.toggle('divActiva')
-    divNuevo.classList.toggle('divEnd')
-
-    setTimeout(function(){divNuevo.remove()},1)
-
-
+}
+function ClickBorrar(){
+  this.classList.remove('divActiva')
+  this.classList.add('divEnd')
+  cambiarColor();
+  setTimeout(function(){
+    const borrar=document.querySelector('.divEnd')
+    borrar.remove();
   }
+  ,200)
+}
+
+function cambiarColor(){
+  const color1 = Math.floor(Math.random()*255)
+  const color2 = Math.floor(Math.random()*255)
+  const color3 = Math.floor(Math.random()*255)
+  const color4 = Math.floor(Math.random()*10)/10
   
-})})
-
-
+  body.style.backgroundColor= `rgb(${color1}, ${color2}, ${color3}, ${color4})`
+}
